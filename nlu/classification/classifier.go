1f7f1f91059acfcc1cec4ec27edec48e3c4564e0
Brandon Atkinson <tehremo@gmail.com>
2022-04-02 11:19:11 -0400

    Comments

--- nlu/classification/classifier.go
+++ nlu/classification/classifier.go
@@ -6,25 +6,32 @@ import (
 	"github.com/navossoc/bayesian"
 )
 
+// ClassifySentence loads a pre-trained model and classifies the supplied sentence
 func ClassifySentence(sentence string) string {
+	// load the pre-trained model
 	classifier, err := LoadClassifier()
 	if err != nil {
 		fmt.Printf("ERROR OCCURED LOADING CLASSIFIER: %s", err)
 	}
 
+	// split the sentence into cleaned words
 	cleanedSentence := SentencesToWords([]string{sentence})
 
+	// perform classification on the words
 	scores, likely, _ := classifier.LogScores(
 		cleanedSentence,
 	)
 
+	// print the results of classification
 	fmt.Printf("Scores: %.2f \n", scores)
 	fmt.Printf("Likely Index: %d, Class: %s \n", likely, classifier.Classes[likely])
 
 	return string(classifier.Classes[likely])
 }
 
+// LoadClassifier loads a pre-trained classification model in the "models" folder
 func LoadClassifier() (*bayesian.Classifier, error) {
+	// load the pre-trained model from a file
 	classifier, err := bayesian.NewClassifierFromFile("models/classifier")
 	if err != nil {
 		return nil, err
