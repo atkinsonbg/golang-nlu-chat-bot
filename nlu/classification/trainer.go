edb7c4e9785c68b78036077068f93eda157ebeb1
Brandon Atkinson <tehremo@gmail.com>
2022-04-02 11:16:37 -0400

    Comments

--- nlu/classification/trainer.go
+++ nlu/classification/trainer.go
@@ -16,9 +16,12 @@ const (
 	Unknown bayesian.Class = "Unknown"
 )
 
+// Train will train a new classification model based on the JSON data in the "data/intents" folder
 func Train(testPhrase []string) {
+	// create a new classifier from the known intents
 	classifier := bayesian.NewClassifierTfIdf(Order, Hours, Unknown)
 
+	// load all the intent data from the JSON files
 	hoursIntents, err := GetIntents("data/intents/hours.json")
 	if err != nil {
 		log.Println(err)
@@ -34,25 +37,34 @@ func Train(testPhrase []string) {
 		log.Println(err)
 	}
 
+	// train the classifier on the loaded intents
 	classifier.Learn(orderIntents, Order)
 	classifier.Learn(hoursIntents, Hours)
 	classifier.Learn(unknownIntents, Unknown)
 
+	// convert terms freq to TFIDF
 	classifier.ConvertTermsFreqToTfIdf()
 
+	// save the trained classifier
 	classifier.WriteToFile("models/classifier")
 
+	// test the model on a supplied test sentence
 	fmt.Printf("TEST SENTENCE: %s\n", testPhrase)
+
 	cleanedTest := SentencesToWords(testPhrase)
 	fmt.Printf("CLEANED SENTENCE: %s\n", cleanedTest)
+
 	scores, likely, _ := classifier.LogScores(
 		cleanedTest,
 	)
 
+	// print the results
 	fmt.Printf("Scores: %.2f \n", scores)
 	fmt.Printf("Likely Index: %d, Class: %s \n", likely, classifier.Classes[likely])
 }
 
+// GetIntents takes a supplied filename, JSON file, and reads its contents, running the sentences
+// through the SentenceToWords util and returns a string array of words
 func GetIntents(filename string) ([]string, error) {
 	jsonFile, err := os.Open(filename)
 	if err != nil {
